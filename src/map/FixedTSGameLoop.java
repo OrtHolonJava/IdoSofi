0941ee8f241b1e055b146c63321bbfa654670ea2
USER <USER@IDO-PC>
2018-01-22 21:07:19 +0200

    2201

---
+++ src/map/FixedTSGameLoop.java
@@ -0,0 +1,60 @@
+package map;
+
+public class FixedTSGameLoop implements Runnable
+{
+	private MapPanel _gamePanel;
+	
+	public FixedTSGameLoop(MapPanel panel) 
+	{
+		this._gamePanel = panel;
+	}
+
+	@Override
+	public void run() 
+	{
+		long lastTime = System.nanoTime(), now;
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+		long timer = System.currentTimeMillis();
+		int updates = 0;
+		int frames = 0;
+		while (this._gamePanel.isRunning())
+		{
+			now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1)
+			{
+				tick();
+				updates++;
+				render();
+				frames++;
+				delta--;
+			}
+			if (System.currentTimeMillis() - timer > 1000)
+			{
+				timer += 1000;
+				System.out.println("FPS: " + frames + " TICKS: " + updates);
+				frames = 0;
+				updates = 0;
+			}
+		}
+	}
+	
+	private void tick()
+	{
+		/**
+		 * Logic goes here:
+		 */
+		this._gamePanel.setLogic();
+	}
+	
+	private void render()
+	{
+		/**
+		 * Rendering the map panel
+		 */
+		this._gamePanel.repaint();
+	}
+}
